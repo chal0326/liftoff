97eb8b137d6ea621d57f7f1c14e37bdfba2c8c08
rawdnyc <rawdnyc@gmail.com>
2023-04-22 07:46:08 -0400

    added config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), partytown()]
+  integrations: [react(), tailwind(), partytown(), image()]
 });
